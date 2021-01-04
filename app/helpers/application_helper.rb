1b2db9b0b096a60326fc4b466205e8298e872f2f
Agnese Portera <agneseportera@gmail.com>
2021-01-04 11:17:09 +0100

    user show page with gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def gravatar_for(user, options = {size: 80})
+    email_address = user.email
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+  end
+
 end
